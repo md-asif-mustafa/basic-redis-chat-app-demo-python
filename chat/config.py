cf44838a62bfafc6c4bd4aa48519d0e5e60a840c
md-asif-mustafa <mdasif.april@gmail.com>
2024-07-19 16:31:09 +0530

    updated docker compose,config.py,redis.conf

--- chat/config.py
+++ chat/config.py
@@ -3,24 +3,21 @@ import redis
 from werkzeug.utils import import_string
 
 class Config(object):
-    # Parse redis environment variables.
     REDIS_HOST = os.environ.get("REDIS_HOST", "127.0.0.1")
     REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
     REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
     SECRET_KEY = os.environ.get("SECRET_KEY", "Optional default value")
     SESSION_TYPE = "redis"
-    redis_client = redis.Redis(
-        host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD
+    redis_client = redis.StrictRedis(
+        host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, decode_responses=True
     )
     SESSION_REDIS = redis_client
-    # TODO: Auth...
 
 class ConfigDev(Config):
-    # DEBUG = True
     pass
 
 class ConfigProd(Config):
     pass
 
 def get_config() -> Config:
-    return import_string(os.environ.get("CHAT_CONFIG", "chat.config.ConfigDev"))
+    return import_string(os.environ.get("CHAT_CONFIG", "chat.config.ConfigDev"))()
