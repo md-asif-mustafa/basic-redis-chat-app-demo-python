61e89d555cd50a74e19dfa587e7f52585341fb91
md-asif-mustafa <mdasif.april@gmail.com>
2024-07-20 10:18:49 +0530

    updated conf.py file with password

--- chat/config.py
+++ chat/config.py
@@ -3,13 +3,14 @@ import redis
 from werkzeug.utils import import_string
 
 class Config(object):
-    REDIS_HOST = os.environ.get("REDIS_HOST", "127.0.0.1")
+    # Parse redis environment variables.
+    REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
     REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
     REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "password")
     SECRET_KEY = os.environ.get("SECRET_KEY", "Optional default value")
     SESSION_TYPE = "redis"
-    redis_client = redis.StrictRedis(
-        host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, decode_responses=True
+    redis_client = redis.Redis(
+        host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD
     )
     SESSION_REDIS = redis_client
 
@@ -20,4 +21,4 @@ class ConfigProd(Config):
     pass
 
 def get_config() -> Config:
-    return import_string(os.environ.get("CHAT_CONFIG", "chat.config.ConfigDev"))()
+    return import_string(os.environ.get("CHAT_CONFIG", "chat.config.ConfigDev"))
